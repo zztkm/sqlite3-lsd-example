0b2092c990e81f7ef870da4f6c9f784e3323163c
TakumiTsuruta <ttsuruta@landscape.co.jp>
2023-01-26 15:57:01 +0900

    update

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ con.create_function("lsd", 2, distance)
 
 cur = con.cursor()
 cur = cur.execute(create_table_sql)
-cur.execute('INSERT INTO user(name) VALUES("zztkm")')
+cur.execute('INSERT INTO user(name) VALUES("藤波 辰爾")')
 cur.execute('INSERT INTO user(name) VALUES("ジャンボ鶴田")')
 cur.execute('SELECT * FROM user')
 print("all data")
@@ -23,7 +23,7 @@ cur.close()
 cur = con.cursor()
 print()
 print("lsd check data")
-cur.execute('SELECT * FROM user WHERE lsd(name, "tkm") < 3')
+cur.execute('SELECT * FROM user WHERE lsd(name, "ジャンボ重田") < 3')
 
 print(cur.fetchall())
 
